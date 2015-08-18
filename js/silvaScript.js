50f481be306cc093f915a8f67db70f8268451dbb
abdelrahman2 <abdelrahman.mohsen.ali@live.com>
2015-08-18 23:36:48 +0200

    Init commit

---
+++ js/silvaScript.js
@@ -0,0 +1,181 @@
+
+var userIuput = document.getElementById("input"),
+   userOutput = document.getElementById("output");
+
+function my() {
+    
+ "use strict";
+    
+    
+userOutput.innerHTML =
+"-webkit-" + userIuput.value +"<br />"+
+"-mos-" + userIuput.value+"<br />"+
+"-ms-" + userIuput.value+"<br />"+
+"-o-" + userIuput.value+"<br />"+
+userIuput.value;
+ 
+    
+}
+/*"-mos-" + x.value
+"-ms-" + x.value
+"-o-" + x.value
+x.value; */ 
+
+
+
+
+
+
+//Array check
+/*if (Array.isArray(x)) {
+
+alert("yes");
+
+
+} else {
+
+
+alert("no");
+.constructor === Array
+
+
+console.log(y.length);
+x.length = 5;
+
+};*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*$(document).ready(function()
+{
+  $("button").mouseenter(function()
+{
+     $(this).css("background-color","#f3456c");
+})
+    $("button").mouseleave(function()
+{
+     $(this).css("background-color","#fff");
+})
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(function()
+{	$(".b1").click(function()
+{	$("body").css("background-color","#4aa3df");
+})
+});
+
+$(function()
+{	$(".b2").click(function()
+{	$("body").css("background-color","#e74c3c");
+})
+});
+
+$(function()
+{	$(".b3").click(function()
+{	$("body").css("background-color","#8e44ad");
+})
+});
+
+
+
+	$("button").dblclick(function()
+
+{
+	$("body").css("background-color","#e74c3c");
+})
+
+	$("button").dblclick(function()
+
+{
+	$("body").css("background-color","#e74c3c");
+})
+
+	$("button").dblclick(function()
+
+{
+	$("body").css("background-color","#e74c3c");
+})
+
+	$("button").dblclick(function()
+
+{
+	$("body").css("background-color","#e74c3c");
+})
+*/
